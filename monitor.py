c3ceb55358358adf63c9d87595b3b2091c11962f
Phadate <fayoded@gmail.com>
2026-01-26 09:36:46 +0100

    feat: Add a starting header structure

--- monitor.py
+++ monitor.py
@@ -1,4 +1,39 @@
-#!/bin/sh
-import datetime
+#! /usr/bin/env python3
+import subprocess
+import sys
+from datetime import datetime
 
-print(datetime.datetime.now())
+current_time = datetime.now().strftime('%d-%m-%Y %X')
+
+# create a header that shows the welcome message
+def header():
+    print("=" * 50)
+    print("SYSTEM HEALTH MONITOR")
+    print(f"Report Time: {current_time}")
+    print("=" * 50)
+
+def get_cpu_usage():
+
+    result = subprocess.run(
+        args=['top', '-bn1'], 
+        capture_output=True, 
+        text=True
+        )
+    
+    for line in result.stdout.split("\n"):
+        if "Cpu(s)" in line:
+            # Treat the comma "," in the string, most time, 
+            cpu_parts = line.split(',')
+            idle = cpu_parts[3].split()[0]
+            print(100 -float(idle))
+            
+
+    # print(result.stdout
+
+def main():
+    header()
+    get_cpu_usage()
+
+
+if __name__ == '__main__':
+    main()
